b2b961e33bee586c9137ad64068ed8327ddba43a
yasaswikankanala <41733919+yasaswikankanala@users.noreply.github.com>
2018-07-26 22:04:22 -0700

    Create 4.java

---
+++ 4.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		char ch;
+		Scanner Sc=new Scanner(System.in);
+		ch=Sc.next().charAt(0);
+		if((ch>='a'&&ch<='z') || (ch>='A'&&ch<='Z'))
+		{
+			System.out.println("Alphabet");
+		}
+		else
+		System.out.println("Not");
+	}
+}
