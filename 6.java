718fdea5f3324d18a45b67726a7ecd624c14e953
yasaswikankanala <41733919+yasaswikankanala@users.noreply.github.com>
2018-07-26 22:39:46 -0700

    Create 6.java

---
+++ 6.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n1;
+		Scanner Sc=new Scanner(System.in);
+		n1=Sc.nextInt();
+		if(n1%4==0)
+		{
+			System.out.println("Yes");
+		}
+		else
+		System.out.println("No");
+	}
+}
