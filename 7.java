d519d0a56b11e3b2434ea1c8e166569b59f7394f
yasaswikankanala <41733919+yasaswikankanala@users.noreply.github.com>
2018-07-26 23:16:22 -0700

    Create 7.java

---
+++ 7.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		String str="Hello";
+		int n,i;
+		Scanner Sc=new Scanner(System.in);
+		n=Sc.nextInt();
+		for(i=1;i<=n;i++)
+		{
+			System.out.println("Hello");
+		}
+		
+	}
+}
