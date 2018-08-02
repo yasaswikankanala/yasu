6d0c9aea318aef47ad975c8d03f6a0d4b0d83b14
yasaswikankanala <41733919+yasaswikankanala@users.noreply.github.com>
2018-08-02 03:24:04 -0700

    Create 15.java

---
+++ 15.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int s,e,i;
+		Scanner Sc=new Scanner(System.in);
+		s=Sc.nextInt();
+		e=Sc.nextInt();
+		for(i=s+1;i<e;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println(i+"\t");
+			}
+		}
+		
+	}
+}
