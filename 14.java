06af04f15b75dee17d94ca736a35bdc6376a6439
yasaswikankanala <41733919+yasaswikankanala@users.noreply.github.com>
2018-08-02 02:13:14 -0700

    Create 14.java

---
+++ 14.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int s,e;
+	Scanner Sc=new Scanner(System.in);
+	s=Sc.nextInt();
+	e=Sc.nextInt();
+	for(int i=s+1;i<=e;i++)
+	{
+		if(i%2!=0)
+		{
+			System.out.println(i+"\t");
+		}
+	}
+	}
